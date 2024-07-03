c8e86f7b1aa278c01d101398404daf2bbe76fa55
mhussain <mhussain@stellatechnology.com>
2024-07-03 15:40:50 +0500

    Communication between services

--- order-service/src/main/java/com/order/service/OrderServiceApplication.java
+++ order-service/src/main/java/com/order/service/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderServiceApplication{
 
 
